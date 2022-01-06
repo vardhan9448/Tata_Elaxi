94a62fcb4d91485687cc9d0ade903b53c4f59d8f
vardhan9448 <88494297+vardhan9448@users.noreply.github.com>
2022-01-06 17:24:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export const App = () => {
     state=4;
     return (
         <div>
-           <ReactPlayer url="https://youtu.be/7sDY4m8KNLc" controls onReady={()=>console.log('started')}/>
+           <ReactPlayer url="https://youtu.be/7sDY4m8KNLc" controls onReady={()=>console.log('started')}/>//hello
        {state} </div>
     )
 }
